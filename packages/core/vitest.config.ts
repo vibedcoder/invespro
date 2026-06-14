e368cc54a96efdead4965b5d58256c04f9fb22d7
zaya <dudezaya@gmail.com>
2026-06-14 23:05:06 +1000

    fix: export conditions

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: false,
     environment: 'node',
-  },
-  resolve: {
-    conditions: ['import', 'require', 'default'],
   },
 });
